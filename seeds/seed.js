4fef88f0833ffd57e418ee3e01ebc09ef3f7686c
Jonathan Olmon <jonathan.olmon@gmail.com>
2021-09-13 18:36:57 -0500

    Added some seeds to test out the layout/funcitonality. Got it to where you can navigate the website and it will display posts/comments appropriately. Need to work on the user dashboard still and then overall formatting.

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,9 @@
 const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const { User, Post, Comment } = require('../models');
 
 const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json')
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -12,10 +13,19 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
+  const posts = [];
+  for (const post of postData) {
+    posts.push(await Post.create({
+      ...post,
       user_id: users[Math.floor(Math.random() * users.length)].id,
+    }));
+  }
+
+  for (const comment of commentData) {
+    await Comment.create({
+      ...comment,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+      post_id: posts[Math.floor(Math.random() * posts.length)].id
     });
   }
 
