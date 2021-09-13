ad8516dec8b6f77ddd4d821253ba009f97cb6242
Jonathan Olmon <jonathan.olmon@gmail.com>
2021-09-13 13:11:20 -0500

    Initial commit with generaric folder/file structure.

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 const { Project } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.post('/', withAuth, async (req, res) => {
+router.use(withAuth);
+
+router.post('/', async (req, res) => {
   try {
     const newProject = await Project.create({
       ...req.body,
@@ -15,7 +17,7 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
-router.delete('/:id', withAuth, async (req, res) => {
+router.delete('/:id', async (req, res) => {
   try {
     const projectData = await Project.destroy({
       where: {
