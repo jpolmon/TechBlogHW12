3a3e411d27d2cde13d19d828a2d6427dc6c00a01
Jonathan Olmon <jonathan.olmon@gmail.com>
2021-09-14 17:59:28 -0500

    Fixed issue with the event listeners for deleting comments/posts

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -17,24 +17,18 @@ router.post('/', async (req, res) => {
   }
 });
 
-// router.delete('/:id', async (req, res) => {
-//   try {
-//     const commentData = await Comment.destroy({
-//       where: {
-//         id: req.params.id,
-//         user_id: req.session.user_id,
-//       },
-//     });
+router.delete('/:id', async (req, res) => {
+  try {
+    const commentData = await Comment.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
 
-//     if (!commentData) {
-//       res.status(404).json({ message: 'No comment found with this id!' });
-//       return;
-//     }
-
-//     res.status(200).json(commentData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -5,7 +5,7 @@ const newFormHandler = async (event) => {
   const content = document.querySelector('#post-content').value.trim();
 
   if (name && content) {
-    const response = await fetch(`/api/posts/`, {
+    const response = await fetch(`/api/posts`, {
       method: 'POST',
       body: JSON.stringify({ name, content }),
       headers: {
@@ -21,18 +21,40 @@ const newFormHandler = async (event) => {
   }
 };
 
-const delButtonHandler = async (event) => {
-    const id = event.target.getAttribute('data-id');
+const postDelButtonHandler = async (event) => {
+  const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/api/posts/${id}`, {
-      method: 'DELETE',
-    });
+  const response = await fetch(`/api/posts/${id}`, {
+    method: 'DELETE',
+  });
 
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert('Failed to delete project');
-    }
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert('Failed to delete project');
+  }
+
+  if (!document.querySelector('.postDelete')) {
+    document.removeEventListener('click', postDelButtonHandler);
+  }
+};
+
+const commentDelButtonHandler = async (event) => {
+  let id = event.target.getAttribute('data-id');
+
+  const response = await fetch(`/api/comments/${id}`, {
+    method: 'DELETE',
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert('Failed to delete comment');
+  }
+
+  if (!document.querySelector('.commentDelete')) {
+    document.removeEventListener('click', commentDelButtonHandler);
+  }
 };
 
 const editButtonHandler = async (event) => {
@@ -55,14 +77,39 @@ const editButtonHandler = async (event) => {
 }
 
 
+
+if (document.querySelector('.postDelete')) {
+  document
+    .querySelector('.postDelete')
+    .addEventListener('click', postDelButtonHandler);
+} 
+// else {
+//   document
+//     .querySelector('.postDelete')
+//     .removeEventListener('click', postDelButtonHandler);
+// }
+
+if (document.querySelector('.commentDelete')) {
+  document
+    .querySelector('.commentDelete')
+    .addEventListener('click', commentDelButtonHandler);
+} 
+// else {
+//   document
+//     .querySelector('.commentDelete')
+//     .removeEventListener('click', postDelButtonHandler);
+// }
+
+
 document
   .querySelector('.new-post-form')
   .addEventListener('submit', newFormHandler);
 
-document
-  .querySelector('#edit')
-  .addEventListener('click', editButtonHandler);
+// document
+//   .querySelector('#postEdit')
+//   .addEventListener('click', editButtonHandler);
 
-document
-  .querySelector('#delete')
-  .addEventListener('click', delButtonHandler);
+// document
+//   .querySelector('#commentEdit')
+//   .addEventListener('click', editButtonHandler);
+
